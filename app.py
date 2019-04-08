d85d9a014a56ac98abd7e5ae59923a4374debe0a
jordanyazzie <jordanyazzie@mail.weber.edu>
2019-04-07 21:47:32 -0600

    Book Review 2 complete

--- app.py
+++ app.py
@@ -1,6 +1,40 @@
+import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Category(db.Model):
+    __tablename__ = 'category'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    content = db.Column(db.Text)
+
+
+    def __repr__(self):
+        return '< Category %r >' % self.name
+
+class Books(db.Model):
+    __tablename__ = 'books'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64), unique=True, index=True)
+    author = db.Column(db.String(64))
+    description = db.Column(db.Text)
+
+    def __repr__( self):
+        return '< Books %r >' % self.title
+
+
+
+
+
+
 
 @app.route('/')
 def index():
